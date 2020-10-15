662b7db2c7fd3661f56bba3d21e6211e948f8e7b
AL <t07902301@csie.ntu.edu.tw>
2020-10-15 14:04:00 +0800

    app file

---
+++ app.js
@@ -0,0 +1,33 @@
+//app.js
+//app.js
+App({
+  data:{
+    all:null,
+    gif:null,
+    name:null,
+    pwd:null,
+    mail:null,
+    source:null,
+
+  },
+  //onLaunch,onShow: options(path,query,scene,shareTicket,referrerInfo(appId,extraData))
+  onLaunch: function(options){
+   
+  },
+  onShow: function(options){
+
+  },
+  onHide: function(){
+
+  },
+  onError: function(msg){
+
+  },
+  //options(path,query,isEntryPage)
+  onPageNotFound: function(options){
+
+  },
+ 
+  
+});
+
